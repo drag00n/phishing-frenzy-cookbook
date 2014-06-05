c3ffbfc24d75ce35297f8afb31a505592136f487
drag00n <christophermluciano@gmail.com>
2014-06-05 11:00:43 -0400

    add apache as dep in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,10 @@
-name             'phishing-frenzy-cookbook'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures phishing-frenzy-cookbook'
-long_description 'Installs/Configures phishing-frenzy-cookbook'
-version          '0.1.0'
+name             'phishing-frenzy'
+maintainer       "Christopher Luciano"
+maintainer_email "christophermluciano@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures phishing-frenzy"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
 
+depends "apache2"
+depends "mysql"
